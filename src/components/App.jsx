aca7f0d0fa6ac86ebfda04d5ae9a772ee15f23f1
orch20 <94862153+orch20@users.noreply.github.com>
2022-08-14 16:26:47 +0300

    refactored App to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import { Loader } from './Loader/Loader';
 import { ImageGallery } from './ImageGallery/ImageGallery';
@@ -8,26 +8,26 @@ import { ToastContainer, toast } from 'react-toastify';
 import { imageService } from '../services/serviceApi.js';
 import styled from 'styled-components';
 
-export class App extends Component {
-  state = {
-    query: '',
-    page: 1,
-    images: [],
-    isLoading: false,
-    isVisible: false,
-    isModal: false,
-    modalURL: '',
-    modalAlt: '',
-  };
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+  const [isModal, setIsModal] = useState(false);
+  const [modalURL, setModalURL] = useState('');
+  const [modalAlt, setModalAlt] = useState('');
+  const [error, setError] = useState(null);
 
-  componentDidUpdate(_, prevState) {
-    const { query, page } = this.state;
-    if (prevState.query !== query || prevState.page !== page) {
-      this.getImages(query, page);
+  useEffect(() => {
+    if (!query) {
+      return;
     }
-  }
-  getImages = async (query, page) => {
-    this.setState({ isLoading: true });
+    getImages(query, page);
+  }, [query, page]);
+
+  const getImages = async (query, page) => {
+    setIsLoading(true);
     try {
       const { hits, totalHits } = await imageService(query, page);
 
@@ -35,81 +35,165 @@ export class App extends Component {
         toast.warn('Wrong query!', { autoClose: 3000 });
       }
 
-      this.setState(prevState => ({
-        images: [...prevState.images, ...hits],
-        isVisible: totalHits > 12 * page,
-      }));
+      setImages([...images, ...hits]);
+      setIsVisible(totalHits > 12 * page);
     } catch (error) {
       console.error(error);
       this.setState({ error: error.message });
     } finally {
-      this.setState({ isLoading: false });
+      setIsLoading(false);
     }
   };
 
-  onHandleSubmit = value => {
-    if (value === this.state.query) {
+  const onHandleSubmit = value => {
+    if (value === query) {
       return;
     }
-    this.setState({
-      query: value,
-      page: 1,
-      images: [],
-      isVisible: false,
-      isEmpty: false,
-      error: null,
-    });
+    setImages([]);
+    setQuery(value);
+    setPage(1);
+    setIsVisible(false);
+    // setIsEmpty(false);
+    setError(null);
   };
 
-  onHandelLoadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const onHandelLoadMore = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  openModal = (url, tag) => {
-    this.setState({
-      isModal: true,
-      modalURL: { url },
-      modalAlt: { tag },
-    });
+  const openModal = (url, tag) => {
+    setIsModal(true);
+    setModalURL(url);
+    setModalAlt(tag);
   };
 
-  closeModal = () => {
-    this.setState({
-      isModal: false,
-    });
+  const closeModal = () => {
+    setIsModal(false);
   };
 
-  render() {
-    const { isLoading, images, isVisible, error, isModal, modalURL, modalAlt } =
-      this.state;
+  return (
+    <Container>
+      {isModal && (
+        <Modal onClick={closeModal}>
+          <img src={modalURL.url} alt={modalAlt.tag} />
+        </Modal>
+      )}
+      {!isModal && <Searchbar onSubmitApp={onHandleSubmit} />}
+      {isLoading && <Loader lOADING={isLoading} />}
+      <ImageGallery images={images} onClick={openModal} />
+      {isVisible && (
+        <Button onClick={onHandelLoadMore} disabled={isLoading} type="button">
+          {isLoading ? 'Loading...' : 'Load more'}
+        </Button>
+      )}
+      <ToastContainer />
+      {error && <p textAlign="center">Sorry, {error}</p>}
+    </Container>
+  );
+};
 
-    return (
-      <Container>
-        {isModal && (
-          <Modal onClick={this.closeModal}>
-            <img src={modalURL.url} alt={modalAlt.tag} />
-          </Modal>
-        )}
-        {!isModal && <Searchbar onSubmitApp={this.onHandleSubmit} />}
-        {isLoading && <Loader lOADING={isLoading} />}
-        <ImageGallery images={images} onClick={this.openModal} />
-        {isVisible && (
-          <Button
-            onClick={this.onHandelLoadMore}
-            disabled={isLoading}
-            type="button"
-          >
-            {isLoading ? 'Loading...' : 'Load more'}
-          </Button>
-        )}
-        <ToastContainer />
-        {error && <p textAlign="center">Sorry, {error}</p>}
-      </Container>
-    );
-  }
-}
+// export class App extends Component {
+//   state = {
+//     query: '',
+//     page: 1,
+//     images: [],
+//     isLoading: false,
+//     isVisible: false,
+//     isModal: false,
+//     modalURL: '',
+//     modalAlt: '',
+//   };
+
+//   componentDidUpdate(_, prevState) {
+//     const { query, page } = this.state;
+//     if (prevState.query !== query || prevState.page !== page) {
+//       this.getImages(query, page);
+//     }
+//   }
+//   getImages = async (query, page) => {
+//     this.setState({ isLoading: true });
+//     try {
+//       const { hits, totalHits } = await imageService(query, page);
+
+//       if (hits.length === 0) {
+//         toast.warn('Wrong query!', { autoClose: 3000 });
+//       }
+
+//       this.setState(prevState => ({
+//         images: [...prevState.images, ...hits],
+//         isVisible: totalHits > 12 * page,
+//       }));
+//     } catch (error) {
+//       console.error(error);
+//       this.setState({ error: error.message });
+//     } finally {
+//       this.setState({ isLoading: false });
+//     }
+//   };
+
+//   onHandleSubmit = value => {
+//     if (value === this.state.query) {
+//       return;
+//     }
+//     this.setState({
+//       query: value,
+//       page: 1,
+//       images: [],
+//       isVisible: false,
+//       isEmpty: false,
+//       error: null,
+//     });
+//   };
+
+//   onHandelLoadMore = () => {
+//     this.setState(prevState => ({
+//       page: prevState.page + 1,
+//     }));
+//   };
+
+//   openModal = (url, tag) => {
+//     this.setState({
+//       isModal: true,
+//       modalURL: { url },
+//       modalAlt: { tag },
+//     });
+//   };
+
+//   closeModal = () => {
+//     this.setState({
+//       isModal: false,
+//     });
+//   };
+
+//   render() {
+//     const { isLoading, images, isVisible, error, isModal, modalURL, modalAlt } =
+//       this.state;
+
+//     return (
+//       <Container>
+//         {isModal && (
+//           <Modal onClick={this.closeModal}>
+//             <img src={modalURL.url} alt={modalAlt.tag} />
+//           </Modal>
+//         )}
+//         {!isModal && <Searchbar onSubmitApp={this.onHandleSubmit} />}
+//         {isLoading && <Loader lOADING={isLoading} />}
+//         <ImageGallery images={images} onClick={this.openModal} />
+//         {isVisible && (
+//           <Button
+//             onClick={this.onHandelLoadMore}
+//             disabled={isLoading}
+//             type="button"
+//           >
+//             {isLoading ? 'Loading...' : 'Load more'}
+//           </Button>
+//         )}
+//         <ToastContainer />
+//         {error && <p textAlign="center">Sorry, {error}</p>}
+//       </Container>
+//     );
+//   }
+// }
 
 const Container = styled.div`
   display: grid;
